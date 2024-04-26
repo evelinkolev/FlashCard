65c415b789057fdff3e03a23f2e9cbe85f8d6236
Evelin Kolev <evelinkolev@vivaldi.net>
2024-04-26 14:44:26 +0300

    Save progress

--- main.py
+++ main.py
@@ -3,11 +3,17 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn = {}
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
-#print(to_learn)
-current_card ={}
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
 
 def next_card():
     global current_card, flip_timer
@@ -19,10 +25,21 @@ def next_card():
     canvas.itemconfig(card_background, image=card_front_img)
     flip_timer = window.after(3000, func=flip_card)
 
+
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    #print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
+
+    next_card()
+
 
 window = Tk()
 window.title("Flashy")
@@ -44,10 +61,9 @@ unknown_button = Button(image=cross_image, command=next_card)
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, command=next_card)
+known_button = Button(image=check_image, command=is_known)
 known_button.grid(row=1, column=1)
 
 next_card()
 
 window.mainloop()
-
